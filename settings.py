726c928f1d08d0cf282a8935fcc6f2a3ad2f7100
rodygayz12 <161971854+rodygayz12@users.noreply.github.com>
2024-04-19 23:21:10 +0400

    Update and rename settings.py.sample to settings.py

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 import os
 
-TOKEN = os.environ.get("TELEGRAM_API_TOKEN", '')
+TOKEN = os.environ.get("TELEGRAM_API_TOKEN", '7106177582:AAEfP44Q0N-9sbVqIRu7jyDmLiGmDlO1c1A')
 
 PORT = int(os.environ.get("PORT", "8443"))
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", "telegram-shell-bot")
 IS_HEROKU = os.environ.get("IS_HEROKU", False)
 
 # Set users to -999999 to disable user authentication
-ENABLED_USERS = os.environ.get("ENABLED_USERS", '')
+ENABLED_USERS = os.environ.get("ENABLED_USERS", '-999999')
 ENABLED_USERS = set(int(e.strip()) for e in ENABLED_USERS.split(','))  # type: ignore
 
 CMD_WHITE_LIST = set()
